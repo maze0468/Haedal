3d76d95b11dc049997f0d86ba3f678d59b7e62dd
Jueyong Lee <leejy0468@naver.com>
2021-06-20 21:27:59 +0900

    python_6_hw.py

--- python/python_6/python_6_hw.py
+++ python/python_6/python_6_hw.py
@@ -3,20 +3,20 @@ import requests
 import csv
 
 headers = {
-    '(User-Agent': 'Not_Crawling X)'
+    'User-Agent': 'Not_Crawling X'
 }
 response = requests.get('https://kin.naver.com/', headers=headers).text
 soup = bs4.BeautifulSoup(response, 'html.parser')
 
 ranks = soup.select('#rankingChart > ul > li')
 
-num_2 = lambda rank : int(rank.select_one('span.no').text)
-title_2 = lambda rank: (rank.select_one('a.ranking_title').text)
+num = lambda rank : int(rank.select_one('span.no').text)
+title = lambda rank: (rank.select_one('a.ranking_title').text)
 
 with open('kin_rank_.csv', 'w') as f:
     writer = csv.writer(f)
-    writer.writerow(['num', 'title'])
-    for rank in sorted(ranks, key=num_2):
-        num = num_2(rank)
-        title = title_2(rank)
-        writer.writerow([f'{num}위,{title}\n'])
+
+    for rank in sorted(ranks, key=num):
+        num2 = num(rank)
+        title2 = title(rank)
+        writer.writerow([f'{num2}위',{title2}])
