62bafde7d79441600e31dba217cabe9b0d43de84
Jueyong Lee <leejy0468@naver.com>
2021-06-20 19:51:57 +0900

    python_6_pw.py

--- python/python_6/python6.py
+++ python/python_6/python6.py
@@ -1,16 +1,16 @@
-import bs4 #pip install bs4
-import requests #pip install requests
+import bs4
+import requests
 
 headers = {
     'User-Agent': 'Not_Crawling X)'
 }
-response = requests. het('https://www.melon.com', headers=headers).text
+response = requests.get('https://www.melon.com', headers=headers).text
 soup = bs4.BeautifulSoup(response, 'html.parser')
 
-songs = soup.select('.on.nth1 > .list_warp.typeRealtime>ul>.rank_item')
+songs = soup.select('.on.nth1 > .list_wrap.typeRealtime>ul>.rank_item')
 
-with open('melon_rank.csv', 'w') as f:
-    for son in songs:
-        title = song.select_one('div.rank_cntt > div.rank_number > span.rank').text
-        artists = sond.select_ont('div.rank_cntt > div.rank_info > div.artist > div.ellipsis > a').text
-        f. write(f'{title},{artist}'\n)
+with open('melon_rank_1.csv', 'w') as f:
+    for song in songs:
+        title = song.select_one('div.rank_cntt > div.rank_info > p.song > a').text
+        artist = song.select_one('div.rank_cntt > div.rank_info > div.artist > div.ellipsis > a').text
+        f. write(f'{title},{artist}\n')
