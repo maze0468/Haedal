1f1bae9464591f1011bd874afe2a012adee403b1
Jueyong Lee <leejy0468@naver.com>
2021-06-20 21:22:47 +0900

    python_6_hw.py

--- python/python_6/python_6_hw.py
+++ python/python_6/python_6_hw.py
@@ -10,10 +10,13 @@ soup = bs4.BeautifulSoup(response, 'html.parser')
 
 ranks = soup.select('#rankingChart > ul > li')
 
+num_2 = lambda rank : int(rank.select_one('span.no').text)
+title_2 = lambda rank: (rank.select_one('a.ranking_title').text)
+
 with open('kin_rank_.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['num', 'title'])
-    for rank in ranks:
-        num = rank.select_one('span.no').text
-        title = rank.select_one('a.ranking_title').text
+    for rank in sorted(ranks, key=num_2):
+        num = num_2(rank)
+        title = title_2(rank)
         writer.writerow([f'{num}위,{title}\n'])
